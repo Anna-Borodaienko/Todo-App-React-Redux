ab5cc1bcd5a9001efd23de0d88c48872d3d5ad09
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-03-06 13:10:59 +0200

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { AppWrapper, Title, Container, Header } from './App.styled'
 import TodoList from './components/TodoList'
 import { AddTodoInput } from './components/AddTodoInput'
 import ToggleAllButton from './components/ToggleAllButton'
-import { useSelector } from 'react-redux'
 import Footer from './components/Footer'
 
 const App: React.FC = (): JSX.Element => {
-  const { todos, fieldForFilter } = useSelector((state: any) => state.todos)
-
-  const filteredTodos = useMemo(() => {
-    return todos.filter((todo: any) => {
-      switch (fieldForFilter) {
-        case 'Active':
-          return !todo.completed
-
-        case 'Completed':
-          return todo.completed
-
-        case 'All':
-        default:
-          return true
-      }
-    })
-  }, [todos, fieldForFilter])
 
   return (
     <AppWrapper>
@@ -33,7 +15,7 @@ const App: React.FC = (): JSX.Element => {
           <ToggleAllButton />
           <AddTodoInput />
         </Header>
-        <TodoList filteredTodos={filteredTodos} />
+        <TodoList />
         <Footer />
       </Container>
     </AppWrapper>
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,18 +1,31 @@
 import { Wrapper } from './TodoList.styled'
-import React from 'react'
-import { Todo } from '../../types/Todo'
+import React, { useMemo } from 'react'
 import TodoCard from '../TodoCard'
+import { useSelector } from 'react-redux'
+import { Todo } from '../../types/Todo'
 
-interface TodoListProps {
-  filteredTodos: Todo[]
-}
+const TodoList: React.FC = (): JSX.Element => {
+  const { todos, fieldForFilter } = useSelector((state: any) => state.todos)
 
-const TodoList: React.FC<TodoListProps> = ({
-  filteredTodos,
-}: TodoListProps): JSX.Element => {
+  const filteredTodos = useMemo(() => {
+    return todos.filter((todo: Todo) => {
+      switch (fieldForFilter) {
+        case 'Active':
+          return !todo.completed
+
+        case 'Completed':
+          return todo.completed
+
+        case 'All':
+        default:
+          return true
+      }
+    })
+  }, [todos, fieldForFilter])
+
   return (
     <Wrapper>
-      {filteredTodos.map((todo) => 
+      {filteredTodos.map((todo: Todo) => 
         <TodoCard key={todo.id} todo={todo} />
       )}
     </Wrapper>
